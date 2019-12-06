93b7ae09abda22d191fc3b6e5719f19be13f63e2
Gianluca Recchia <gianluca.recchia97@gmail.com>
2019-12-06 09:50:15 +0100

    Add user.js containing Firefox preferences

---
+++ .config/user.js
@@ -0,0 +1,27 @@
+// My preferences
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.capacity", 42000);
+user_pref("extensions.pocket.enabled", false);
+user_pref("gfx.canvas.azure.accelerated", true);
+user_pref("gfx.webrender.all", true);
+user_pref("layers.acceleration.force-enabled", true);
+
+// Smooth physics-based scrolling in Firefox
+user_pref("general.smoothScroll.lines.durationMaxMS", 125);
+user_pref("general.smoothScroll.lines.durationMinMS", 125);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.other.durationMaxMS", 125);
+user_pref("general.smoothScroll.other.durationMinMS", 125);
+user_pref("general.smoothScroll.pages.durationMaxMS", 125);
+user_pref("general.smoothScroll.pages.durationMinMS", 125);
+
+// Additionally the mouse wheel scroll settings have to be changed to react in
+// a smooth way as well
+user_pref("mousewheel.min_line_scroll_amount", 30);
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
+user_pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 175);
+user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 175);
+user_pref("toolkit.scrollbox.horizontalScrollDistance", 6);
+user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
