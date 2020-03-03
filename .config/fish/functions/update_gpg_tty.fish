89d8b58fe4540db249993ffde3aefcdf01a40297
Gianluca Recchia <gianluca.recchia97@gmail.com>
2020-03-03 18:42:30 +0100

    Clean up declaration of GPG_TTY variable

--- .config/fish/functions/update_gpg_tty.fish
+++ .config/fish/functions/update_gpg_tty.fish
@@ -1,6 +1,6 @@
 function update_gpg_tty -d 'Update current tty for the gpg agent'
 	# Set the pseudo-tty for the gpg-agent
-	set -x GPG_TTY (tty)
+	set GPG_TTY (tty)
 
 	# Commit changes to the gpg-agent
 	gpg-connect-agent updatestartuptty /bye >/dev/null
